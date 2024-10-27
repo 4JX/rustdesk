4d31ff79fcb702d144112a6f1b0973de9834f7cb
4JX <4JXcYvmyu3Hz8fV@protonmail.com>
2024-10-27 12:38:37 +0100

    Add logging statements

--- libs/scrap/build.rs
+++ libs/scrap/build.rs
@@ -6,6 +6,8 @@ use std::{
 
 #[cfg(all(target_os = "linux", feature = "linux-pkg-config"))]
 fn link_pkg_config(name: &str) -> Vec<PathBuf> {
+    println!("Using pkg-config");
+    println!("name: {}", name);
     // sometimes an override is needed
     let pc_name = match name {
         "libvpx" => "vpx",
@@ -25,8 +27,11 @@ fn link_pkg_config(_name: &str) -> Vec<PathBuf> {
 
 /// Link vcpkg package.
 fn link_vcpkg(mut path: PathBuf, name: &str) -> PathBuf {
+    println!("link_vcpkg");
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     let mut target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    println!("target_os: {}, target_arch: {}", target_os, target_arch);
+
     if target_arch == "x86_64" {
         target_arch = "x64".to_owned();
     } else if target_arch == "x86" {
@@ -80,6 +85,8 @@ fn link_vcpkg(mut path: PathBuf, name: &str) -> PathBuf {
 fn link_homebrew_m1(name: &str) -> PathBuf {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    println!("target_os: {}, target_arch: {}", target_os, target_arch);
+    
     if target_os != "macos" || target_arch != "aarch64" {
         panic!("Couldn't find VCPKG_ROOT, also can't fallback to homebrew because it's only for macos aarch64.");
     }
@@ -133,14 +140,22 @@ fn link_homebrew_m1(name: &str) -> PathBuf {
 fn find_package(name: &str) -> Vec<PathBuf> {
     let no_pkg_config_var_name = format!("NO_PKG_CONFIG_{name}");
     println!("cargo:rerun-if-env-changed={no_pkg_config_var_name}");
+
+    println!("Checking config for linux-pkg-config feature: {}", cfg!(all(target_os = "linux", feature = "linux-pkg-config")));
+    let no_pkg_config_var_value = std::env::var(no_pkg_config_var_name);
+    println!("Checking NO_PKG_CONFIG variable value: {:?}", no_pkg_config_var_value);
+    
     if cfg!(all(target_os = "linux", feature = "linux-pkg-config"))
-        && std::env::var(no_pkg_config_var_name).as_deref() != Ok("1")
+        && no_pkg_config_var_value.as_deref() != Ok("1")
     {
+        println!("Using pkg-config");
         link_pkg_config(name)
     } else if let Ok(vcpkg_root) = std::env::var("VCPKG_ROOT") {
+        println!("Using vcpkg");
         vec![link_vcpkg(vcpkg_root.into(), name)]
     } else {
         // Try using homebrew
+        println!("Using homebrew");
         vec![link_homebrew_m1(name)]
     }
 }
@@ -152,6 +167,7 @@ fn generate_bindings(
     exact_file: &Path,
     regex: &str,
 ) {
+    println!("Generating bindings for: {}", ffi_header.display());
     let mut b = bindgen::builder()
         .header(ffi_header.to_str().unwrap())
         .allowlist_type(regex)
@@ -171,21 +187,36 @@ fn generate_bindings(
 }
 
 fn gen_vcpkg_package(package: &str, ffi_header: &str, generated: &str, regex: &str) {
+    println!("Generating vcpkg package for: {}", package);
+
     let includes = find_package(package);
+    println!("Includes found: {:?}", includes);
+
     let src_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let src_dir = Path::new(&src_dir);
+    println!("Source directory: {}", src_dir.display());
+
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let out_dir = Path::new(&out_dir);
+    println!("Output directory: {}", out_dir.display());
 
     let ffi_header = src_dir.join("src").join("bindings").join(ffi_header);
+    println!("FFI header path: {}", ffi_header.display());
     println!("rerun-if-changed={}", ffi_header.display());
+
     for dir in &includes {
+        println!("Include directory: {}", dir.display());
         println!("rerun-if-changed={}", dir.display());
     }
 
     let ffi_rs = out_dir.join(generated);
+    println!("Generated FFI Rust source path: {}", ffi_rs.display());
+
     let exact_file = src_dir.join("generated").join(generated);
+    println!("Exact file path: {}", exact_file.display());
+
     generate_bindings(&ffi_header, &includes, &ffi_rs, &exact_file, regex);
+    println!("Bindings generated for package: {}", package);
 }
 
 // If you have problems installing ffmpeg, you can download $VCPKG_ROOT/installed from ci
@@ -235,37 +266,64 @@ fn ffmpeg() {
 */
 
 fn main() {
+    println!("Starting build script...");
+
     // note: all link symbol names in x86 (32-bit) are prefixed wth "_".
     // run "rustup show" to show current default toolchain, if it is stable-x86-pc-windows-msvc,
     // please install x64 toolchain by "rustup toolchain install stable-x86_64-pc-windows-msvc",
     // then set x64 to default by "rustup default stable-x86_64-pc-windows-msvc"
     let target = target_build_utils::TargetInfo::new();
-    if target.unwrap().target_pointer_width() != "64" {
-        // panic!("Only support 64bit system");
-    }
-    env::remove_var("CARGO_CFG_TARGET_FEATURE");
-    env::set_var("CARGO_CFG_TARGET_FEATURE", "crt-static");
+    println!("Target info retrieved.");
 
+    if let Ok(target) = target {
+        println!("Target pointer width: {}", target.target_pointer_width());
+        if target.target_pointer_width() != "64" {
+            println!("Only support 64bit system");
+            // panic!("Only support 64bit system");
+        }
+    } else {
+        println!("Failed to retrieve target info.");
+    }
+
+    env::remove_var("CARGO_CFG_TARGET_FEATURE");
+    println!("Removed CARGO_CFG_TARGET_FEATURE environment variable.");
+    env::set_var("CARGO_CFG_TARGET_FEATURE", "crt-static");
+    println!("Set CARGO_CFG_TARGET_FEATURE to crt-static.");
+
+    println!("Finding package: libyuv");
     find_package("libyuv");
+    println!("Generating vcpkg package: libvpx");
     gen_vcpkg_package("libvpx", "vpx_ffi.h", "vpx_ffi.rs", "^[vV].*");
+    println!("Generating vcpkg package: aom");
     gen_vcpkg_package("aom", "aom_ffi.h", "aom_ffi.rs", "^(aom|AOM|OBU|AV1).*");
+    println!("Generating vcpkg package: libyuv");
     gen_vcpkg_package("libyuv", "yuv_ffi.h", "yuv_ffi.rs", ".*");
+    // println!("Generating ffmpeg package");
     // ffmpeg();
 
     // there is problem with cfg(target_os) in build.rs, so use our workaround
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    println!("Target OS: {}", target_os);
     if target_os == "ios" {
         // nothing
+        println!("Target OS is iOS, doing nothing.");
     } else if target_os == "android" {
+        println!("Target OS is Android, setting cargo cfg to android.");
         println!("cargo:rustc-cfg=android");
     } else if cfg!(windows) {
         // The first choice is Windows because DXGI is amazing.
+        println!("Target OS is Windows, setting cargo cfg to dxgi.");
         println!("cargo:rustc-cfg=dxgi");
     } else if cfg!(target_os = "macos") {
         // Quartz is second because macOS is the (annoying) exception.
+        println!("Target OS is macOS, setting cargo cfg to quartz.");
         println!("cargo:rustc-cfg=quartz");
     } else if cfg!(unix) {
         // On UNIX we pray that X11 (with XCB) is available.
+        println!("Target OS is Unix, setting cargo cfg to x11.");
         println!("cargo:rustc-cfg=x11");
     }
+
+    println!("Build script completed.");
 }
+
